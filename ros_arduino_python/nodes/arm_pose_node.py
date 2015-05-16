03dfb5a42bebd50b5918e4dd3f83a98c2f965e95
theroboticsheep <nate@updroid.com>
2015-05-15 23:53:07 -0700

    updated arm pose node to execute sequences of poses

--- ros_arduino_python/nodes/arm_pose_node.py
+++ ros_arduino_python/nodes/arm_pose_node.py
@@ -9,15 +9,24 @@ import thread
 class ArmPose():
     def __init__(self):
         rospy.init_node('arm_pose', log_level=rospy.DEBUG)
-
-        self.joint_num = rospy.get_param("joint_num", 0)
-        self.joint_angle = rospy.get_param("joint_angle", 0)
         
-        rospy.loginfo("Received joint number " + str(self.joint_num) + " and angle: " + str(self.joint_angle) + " radians")
+        self.animation = rospy.get_param("animation", "~proto-demo")
+        self.sequence = rospy.get_param("sequence", "~sequence1")
+        self.pose_name = rospy.get_param("pose_name", "~home")
+        rospy.loginfo("animation name: " + self.animation)
+        rospy.loginfo("sequence name: " + self.sequence)
+        rospy.loginfo("pose name: " + self.pose_name)
+        
+        self.poses = rospy.get_param(self.animation, dict({}))
         
         rospy.wait_for_service('/arduino/servo_write')
         servo_write = rospy.ServiceProxy('/arduino/servo_write', ServoWrite)
-        response = servo_write(self.joint_num, self.joint_angle)
+        
+        for pose in self.poses[self.sequence]:
+            rospy.loginfo("Pose set: " + pose)
+            for joint, params in self.poses[pose].iteritems():
+                servo_write(params['joint_num'],params['joint_angle'])
+            time.sleep(7)
 
         rospy.loginfo("Command sent to servo")
         rospy.signal_shutdown("Shutting down arm_pose node")
